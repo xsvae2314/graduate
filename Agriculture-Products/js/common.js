d228f17d0e44c5bf910f2e13bd4069fb0f490477
wei <15320012037@163.com>
2019-03-18 23:58:21 +0800

    一些js

--- Agriculture-Products/js/common.js
+++ Agriculture-Products/js/common.js
@@ -1,3 +1,20 @@
 
 
-//  从本地存储获取 购物车商品数量
+//  从本地存储获取 购物车商品数量 导航栏显示
+function getCartCount(){
+	var shoppingCart = localStorage.getItem('shoppingCart');
+	// 本地未检测到存储商品 返回购物车数量为0
+	if(!shoppingCart){
+		return 0;
+	}
+	// 本地检测到存储商品 返回购物车数量为存储的legnth
+	if(shoppingCart){
+		shoppingCart = JSON.parse(shoppingCart);
+		return shoppingCart.length;
+	}
+}
+
+
+
+
+
