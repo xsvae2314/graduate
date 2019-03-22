0423976e443af57574c0d5ab5b54613853f73684
wei <15320012037@163.com>
2019-03-22 20:11:47 +0800

    小吃

--- Snack/js/common.js
+++ Snack/js/common.js
@@ -15,6 +15,10 @@ function getCartCount(){
 }
 
 
-
-
+//  获取地址栏的参数
+function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    };
 
